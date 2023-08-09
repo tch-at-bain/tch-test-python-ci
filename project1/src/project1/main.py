6b6499e1d90ea5c2d73cdaa865b298a7bdc09757
Tomasz Chodakowski <tomasz.chodakowski@bain.com>
2023-08-09 16:06:45 +0100

    feat: added function2

--- project1/src/project1/main.py
+++ project1/src/project1/main.py
@@ -7,6 +7,11 @@ def function1():
     logger.info("In function1")
     return True
 
+def function2():
+    logger.info("In function2")
+    return True
+
+
 
 if __name__ == "__main__":
     print("Hello, World!")
--- project1/tests/unit/test_function1.py
+++ project1/tests/unit/test_function1.py
@@ -4,3 +4,8 @@ from project1 import main
 def test_function1():
     print("testing function1")
     assert main.function1()
+
+
+def test_function2():
+    print("testing function2")
+    assert main.function2()
