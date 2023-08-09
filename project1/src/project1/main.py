0cf7bede9ad28659c5df59728ac0ae1c8024606d
Tomasz Chodakowski <tomasz.chodakowski@bain.com>
2023-08-09 16:17:04 +0100

    feat(fun): modified function1 BREAKING_CHANGE: added argument to function1

--- project1/src/project1/main.py
+++ project1/src/project1/main.py
@@ -3,14 +3,14 @@ import loguru
 logger = loguru.logger
 
 
-def function1():
-    logger.info("In function1")
+def function1(a: int):
+    logger.info(f"In function1 {a=}")
     return True
+
 
 def function2():
     logger.info("In function2")
     return True
-
 
 
 if __name__ == "__main__":
--- project1/tests/unit/test_function1.py
+++ project1/tests/unit/test_function1.py
@@ -3,7 +3,7 @@ from project1 import main
 
 def test_function1():
     print("testing function1")
-    assert main.function1()
+    assert main.function1(1)
 
 
 def test_function2():
